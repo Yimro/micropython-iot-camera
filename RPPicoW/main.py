e1a490e72689d16bbfdf38fe51ea320887ce82fb
jimra <mail@janreitsma.de>
2023-05-25 23:21:17 +0200

    updated main.py

--- RPPicoW/main.py
+++ RPPicoW/main.py
@@ -1,19 +1,16 @@
-from machine import Pin, SPI, RTC, UART
-from ssd1306 import SSD1306_SPI
+from machine import Pin, I2C, RTC, UART
 from umqtt.simple2 import MQTTClient 
 import time, random, math, gc, json, network
-
-#from dht20 import DHT20
 
 #import mqttbasics as mq
 import wificred as wf
 
 # infrared sensor
 pir = Pin(17, Pin.IN, Pin.PULL_DOWN)
-        
-# spi interface, display
-spi = SPI(0, 100000, mosi=Pin(19), sck=Pin(18))
-display = SSD1306_SPI(128, 64, spi, Pin(20), Pin(21), Pin(22))
+
+# display
+display = None
+display_available = False
 
 # mqtt settings
 server="192.168.178.36"
@@ -22,12 +19,6 @@ topic = "proj/camera"
 motion = False
 image_count = 0
 num = 0
-
-# animation values
-x = random.randint(5,123)
-y = random.randint(5,59)
-xdir = random.randrange(-1,1,2)
-ydir = random.randrange(-1,1,2)
 
 # irq handler for PIR Sensor
 def irq_handler(pin):
@@ -39,28 +30,60 @@ def irq_handler(pin):
 # set interrupt on PIR sensor
 pir.irq(handler=irq_handler, trigger=Pin.IRQ_RISING)
 
-def getImage(filename, baudrate):
+def get_image_UART(filename, baudrate):
     uart0 = UART(0, baudrate=baudrate, tx=Pin(0), rx=Pin(1), rxbuf=28000)
-    #time.sleep(1)
+    time.sleep(1)
     uart0.write("start")
-    time.sleep(2)
+    time.sleep(1)
     
-    #rxData = readBytesUart()
     rxData = bytes()
     n  = 0
     while(uart0.any() > 0):
        rxData += uart0.read(1)
        n +=1 
        if (n % 1024 == 0):
-           print(str(n/1024) + " kbytes -")
-    print("uartcam: file size: {} bytes".format(str(n)))
+           print(str(math.floor((n/1024)) + " kbytes -"))
+    print("get_image_UART: File size: {} bytes".format(str(n)))
     #writeToFile(filename, rxData)
     with open(filename, 'wb') as f:
         f.write(rxData)
-        print('uartcam: Image written into {}'.format(filename))
+        print('get_image_UART: Image written into {}'.format(filename))
         f.close()
     uart0.deinit()
     return n
+
+def get_img(baudrate):
+    uart0 = UART(0, baudrate=baudrate, tx=Pin(0), rx=Pin(1), rxbuf=28000)
+    
+    uart0.write("start")
+    time.sleep(3)
+    start_time = time.ticks_ms()
+    file_size = uart0.any()
+    
+    #while(uart0.any() > 0):
+    #   rx += uart0.read(1)
+    if file_size > 0:
+        rx = bytes()
+        for n in range(file_size-1):
+            rx += uart0.read(1)
+            if n % 1024 == 0 and n > 0:
+                print(n)
+    
+    end_time = time.ticks_ms()
+    diff_time = end_time-start_time
+    speed = file_size/diff_time*1000
+    
+    print("file size: {} bytes, transfer time: {} ms, speed: {} bytes/second".format(file_size, diff_time, math.floor(speed)))        
+    
+    
+    '''with open(filename, 'wb') as f:
+        f.write(rx)
+        print('get_image_UART: Image ({} bytes) written into {}'.format(len, filename))
+        f.close()
+    '''
+    uart0.deinit()
+    return rx
+
 
 # publishing files over mqtt
 def pub_file(client, file_name, block_size=2000):
@@ -89,7 +112,8 @@ def pub_file(client, file_name, block_size=2000):
 
 
 def main():
-    global motion, filename, num, x, y, xdir, ydir
+    
+    global motion, filename, num, x, y, xdir, ydir, display_available
     num = 0
     
     #connect to wifi
@@ -101,74 +125,59 @@ def main():
     print(nic.ifconfig())
     
     try:
+        print("main: connecting to {} ...".format(server))
         client = MQTTClient("myclient", server, 1883)
         client.connect()
+        print("main: connected")
     except:
-        time.sleep(5)
-        client.reconnect()
+        print("main: no connection to broker")
         
-    while True:
+    while True:        
         filename = "file"+str(num)+".jpg"
         # motion detected:
         if motion:
             print("main: motion")
-            display.fill(0)
-            display.text(" ALARM!", 30, 9, 1)
-            display.text("saving {}".format(filename), 0, 20, 1)
-            display.show()
-            time.sleep_ms(100)
             
+            print("main: Getting image from camera over UART/I2C")
+            start_time = time.ticks_ms()
             
-            print("main: Getting image from camera over UART")
-            start_uart = time.ticks_ms()
-            size = getImage(filename, 2000000)
-            end_uart = time.ticks_ms()
-            diff_uart = end_uart-start_uart
-            speed_uart = size/diff_uart*1000
+            # request image from ESP32CAM:
+            size = get_img(filename, 2000000)
+            # size = get_image_I2C(i2c, 85)
+
+            end_time = time.ticks_ms()
+            diff_time = end_time-start_time
+            speed = size/diff_time*1000
             print("main: Finished transferring image over UART")
+            print("main: UART transfer time: {} ms; bytes transferred: {}; transfer speed: {}"
+                  .format(str(diff_time), str(size), str(speed)))
             
-            print("main: UART transfer time: {} ms; bytes transferred: {}; transfer speed: {}".format(str(diff_uart), str(size), str(speed_uart)))
-            
-            
-            display.text(" done ", 30, 30, 1)
-            display.show()
-            
+            # publish image via mqtt
             print("main: Start publishing to MQTT broker")
             
             pub_file(client, filename, 500)
             start_pub = time.ticks_ms()
-            print("main: Done publishing to MQTT broker")
+            print("main: Done publishing {} to MQTT broker".format(filename))
             end_pub = time.ticks_ms()
             diff_pub = end_pub-start_pub
             
-            
-            display.text("Published: {}".format(filename), 3, 55, 1)
             #print("main: MQTT publish time: {} ms; bytes transferred: {}".format(str(diff_uart), str(size), str(speed)))
-            
-            display.show()
+                
             #msg0 = "Sent " + filename + " at " + str(time.time()) + " seconds since the epoch"
                         
             time.sleep(5)
             num = (num+1)%10
-            motion = False
+            motion = False       
         
-        # show animation while no motion detected:
-        display.fill(0)
-        display.rect(0,0,128,64,1)
-        display.text("{} image(s)".format(num), 3,3,1)
-        display.ellipse(x,y,5,5, 1)
-        display.show()
-        time.sleep_ms(20)        
-        x += xdir
-        y += ydir
-        if (x == 123):
-            xdir = -1
-        if (x == 5):
-            xdir = 1
-        if (y == 59):
-            ydir = -1
-        if (y == 5):
-            ydir = 1
+#if __name__ == '__main__':
+#   main()
 
-if __name__ == '__main__':
-   main()
+buf = get_img(2000000)
+
+time.sleep(2)
+
+with open("filen.jpg", 'wb') as f:
+    f.write(buf)
+    #print('get_image_UART: Image ({} bytes) written into {}'.format(len, filename))
+    print("file written")
+    f.close()
